31f94f7f46b0ace32aa514c56191c3239d5563f9
Pulipuli Chen <pulipuli.chen@gmail.com>
2024-02-28 16:55:42 +0800

    西元2024年02月28日 (週三) 16時55分42秒 CST

--- assets/setup_url.sh
+++ assets/setup_url.sh
@@ -10,7 +10,7 @@ if [ ! -f "url.txt" ]; then
   # Check if the result is not empty
   if [ -n "$result" ]; then
       # Do something with the result, such as printing it
-      echo "https://pulipulichen.github.io/delopy-tunnel-holder/?r=${result}" > url.txt
+      echo "https://rb.gy/kw7vwr?r=${result}" > url.txt
   else
       echo "Error: No result obtained from the curl command."
   fi
